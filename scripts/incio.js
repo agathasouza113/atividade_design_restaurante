5a7adae02854c4b6e04d0fae01090f74522d15e6
Kalel-Fernandes <kalelfernandes.senai@gmail.com>
2026-02-24 16:14:48 -0300

    commit 24-02

---
+++ scripts/incio.js
@@ -0,0 +1,9 @@
+const modal = document.querySelector('.modal-container1')
+
+function openModal1() {
+    modal.classList.add('active');
+}
+
+function closeModal1() {
+    modal.classList.remove('active');
+}
