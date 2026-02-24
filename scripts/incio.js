2231333811f81163219dad2861815d5c9ca95497
Kalel-Fernandes <kalelfernandes.senai@gmail.com>
2026-02-24 18:48:15 -0300

    Alteração 24-02

--- scripts/incio.js
+++ scripts/incio.js
@@ -1,9 +1,36 @@
-const modal = document.querySelector('.modal-container1')
+const modal1 = document.querySelector('.modal-container1')
+const modal2 = document.querySelector('.modal-container2')
+const modal3 = document.querySelector('.modal-container3')
+const modal4 = document.querySelector('.modal-container4')
 
 function openModal1() {
-    modal.classList.add('active');
+    modal1.classList.add('active');
 }
 
 function closeModal1() {
-    modal.classList.remove('active');
+    modal1.classList.remove('active');
+}
+
+function openModal2() {
+    modal2.classList.add('active');
+}
+
+function closeModal2() {
+    modal2.classList.remove('active');
+}
+
+function openModal3() {
+    modal3.classList.add('active');
+}
+
+function closeModal3() {
+    modal3.classList.remove('active');
+}
+
+function openModal4() {
+    modal4.classList.add('active');
+}
+
+function closeModal4() {
+    modal4.classList.remove('active');
 }
